87790b72a1a2a4c0ace52958bd4f0401e6d1d623
Georgi Parlakov <gparlakov@gmail.com>
2020-03-27 09:56:47 +0200

    replace with example code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { PersonContact } from './person-contact/person-contact.model';
 
 @Component({
-  selector: 'my-app',
+  selector: 'fty-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  title = 'forms-typed';
+  person: PersonContact;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,42 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { PersonContactComponent } from './person-contact/person-contact.component';
+import { PartyFormComponent } from './party-form/party-form.component';
+import { EventFormComponent } from './event-form/event-form.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCommonModule, MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { ShowFormControlModule } from 'show-form-control';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [AppComponent, PersonContactComponent, PartyFormComponent, EventFormComponent],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatCommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ShowFormControlModule.for(environment.production ? 'prod' : 'dev'),
+    MatIconModule,
+    MatDividerModule,
+    MatProgressSpinnerModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
